1de1c0e183d8d4d5b70e7b056f7ba3f44a9f78f5
Mayowa Ayeni <mayeni@udel.edu>
2024-06-21 23:23:24 -0400

    Merged files

--- Fraud_Dectection.py
+++ Fraud_Dectection.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 
 #1a. imported payroll.CSV
-Payroll_df = pd.read_csv('payroll.csv') 
+Payroll_df = pd.read_csv('payroll.csv')
+Postions_df = pd.read_csv('positions.csv') 
 
 # List of columns to drop
 columns_to_drop = ['Project','Unnamed: 9', 'Unnamed: 11', 'Value', 'Unnamed: 13']
@@ -14,4 +14,10 @@ Payroll_df.drop(columns=columns_to_drop, errors='ignore', inplace=True)
 
 # Saves filtered dataset into CSV
 new_payroll_df = Payroll_df.to_csv('New payroll.csv', index= False)
+new_payroll_df = pd.read_csv('New payroll.csv')
 
+# Merge payroll and position by name
+merged_df = pd.merge(new_payroll_df, Postions_df, on= "Name")
+
+# Save the merged dataframe to a new CSV file
+merged_df.to_csv('Merged.csv', index= False)
