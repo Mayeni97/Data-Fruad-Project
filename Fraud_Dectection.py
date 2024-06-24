3eb4e92706a1a3781b1a5f47fcc2eb8c3c55d670
Mayowa Ayeni <mayeni@udel.edu>
2024-06-24 18:22:27 -0400

    Added new column called duration made a new csv file

--- Fraud_Dectection.py
+++ Fraud_Dectection.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 #1a. imported payroll.CSV
 Payroll_df = pd.read_csv('payroll.csv')
-Postions_df = pd.read_csv('positions.csv') 
+Positions_df = pd.read_csv('positions.csv') 
 
 # List of columns to drop
 columns_to_drop = ['Project','Unnamed: 9', 'Unnamed: 11', 'Value', 'Unnamed: 13']
@@ -17,9 +17,9 @@ new_payroll_df = Payroll_df.to_csv('New payroll.csv', index= False)
 new_payroll_df = pd.read_csv('New payroll.csv')
 
 # Merge payroll and position by name
-merged_df = pd.merge(new_payroll_df, Postions_df, on= "Name")
+merged_df = pd.merge(new_payroll_df, Positions_df, on= "Name")
 
-# Save the merged dataframe to a new CSV file
+# Save the merged dataset to a new CSV file
 merged_df.to_csv('Merged.csv', index= False)
 merged_df =  pd.read_csv("Merged.csv")
 
@@ -28,3 +28,14 @@ merged_df["Date"] = pd.to_datetime(merged_df['Date'])
 
 # Filter by date
 filtered_df = merged_df[(merged_df['Date'] >= '2024-02-02') & (merged_df['Date'] <= '2024-05-31')]
+
+# Filter by purpose codes
+filtered_df = filtered_df[(filtered_df['Speed Type'] == 'CISC112828') | (filtered_df['Speed Type'] == 'CISC112822')]
+
+# Adding Duration columns to the dataset
+filtered_df.insert(11, "Duration", 0)
+
+# Saved the the new dataset in a CSV file
+filtered_df.to_csv('Filtered.csv', index= False)
+
+
